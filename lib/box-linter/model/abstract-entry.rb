0c2af9d21d88b5224d674767223d4e5ab9635286
naosuke <info@naosuke.me>
2019-05-02 22:08:50 +0900

    add instance method @description

--- lib/box-linter/model/abstract-entry.rb
+++ lib/box-linter/model/abstract-entry.rb
@@ -1,5 +1,5 @@
 class BoxLinter::Model::AbstractEntry
-  attr_reader :name, :parent, :created_at, :updated_at
+  attr_reader :id, :name, :parent, :description, :created_at, :updated_at
 
   def initialize(result)
     # result => {
@@ -12,6 +12,7 @@ class BoxLinter::Model::AbstractEntry
     @id = result.id.to_i
     @name = result.name
     @parent = result.parent
+    @description = result.description
     @created_at = result.content_created_at
     @updated_at = result.content_modified_at
   end
