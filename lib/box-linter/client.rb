9b614b330903f7b0cbd9ecfeba7bd2dd118da9ee
naosuke <info@naosuke.me>
2019-05-02 17:44:23 +0900

    move Folder.project_root from Client#root

--- lib/box-linter/client.rb
+++ lib/box-linter/client.rb
@@ -1,18 +1,11 @@
 class BoxLinter::Client
-  def initialize
-    @client = Boxr::Client.new
-  end
-
-  def root
-    BoxLinter::EntryFactory.generate @client.folder_from_id(ENV['BOX_PROJECT_ROOT'])
-  end
-
-  def find(id, type='folder')
+  def self.find(id, type='folder')
+    client = Boxr::Client.new
     result = case type
              when 'folder'
-               @client.folder_from_id(id)
+               client.folder_from_id(id)
              when 'file'
-               @client.file_from_id(id)
+               client.file_from_id(id)
              else
                raise NotImplementedError
              end
--- lib/box-linter/model/folder.rb
+++ lib/box-linter/model/folder.rb
@@ -1,4 +1,8 @@
 class BoxLinter::Model::Folder < BoxLinter::Model::AbstractEntry
+  def self.project_root
+    BoxLinter::Client.find(ENV['BOX_PROJECT_ROOT'], 'folder')
+  end
+
   def initialize(result)
     super(result)
   end
